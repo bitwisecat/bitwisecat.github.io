627d1e22eb33178ffca35485af948ac5a1b663e1
bitwisecat <bitwisecat@gmail.com>
2023-02-20 22:42:46 +0300

    add comment to images

--- components/Image.js
+++ components/Image.js
@@ -1,6 +1,10 @@
-import NextImage from 'next/image'
+// import NextImage from 'next/image'
 
-// eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />
+// const myLoader = ({ src, width, quality }) => {
+//     return `https://bitwisecat.github.io/${src}?w=${width}&q=${quality || 75}`
+//   }
+  
+// // eslint-disable-next-line jsx-a11y/alt-text
+// const Image = ({ ...rest }) => <NextImage {...rest} />
 
-export default Image
+// export default Image
